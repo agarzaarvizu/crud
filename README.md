ef7570f07fd797cf6eb27f905797f9e8b246178f
Alex Reiger <hagarza@gmail.com>
2022-09-07 10:02:11 -0500

    [ADD] app: crud creation

--- README.md
+++
@@ -1,6 +0,0 @@
-# flask_template
-This is a simple template in order to use flask
-
-In order to execute the app, you must execute the command:
-
-flask --app <app.name> run
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__, template_folder="views")
+app.secret_key = "75fc78df8vhj92gv92yvhz"
+
 
 from app.controllers import *
+
+if __name__ == "__main__":
+    app.run(debug=True)
